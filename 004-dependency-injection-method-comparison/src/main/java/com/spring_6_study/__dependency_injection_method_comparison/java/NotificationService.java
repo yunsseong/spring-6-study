2409dabfebb67584a20316a05192b378b266aed2
yunsseong <aster20@hanyang.ac.kr>
2025-02-28 00:29:31 +0900

    chore: 파일 위치 이동

--- 004-dependency-injection-method-comparison/src/main/java/com/spring_6_study/__dependency_injection_method_comparison/java/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.spring_6_study.__dependency_injection_method_comparison.java;
-
-public class NotificationService {
-    public void sendNotification(String message) {
-        System.out.println("알림 발송 : " + message);
-    }
-}
