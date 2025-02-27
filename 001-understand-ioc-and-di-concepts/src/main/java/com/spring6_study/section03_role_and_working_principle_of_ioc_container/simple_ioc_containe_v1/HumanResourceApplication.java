a7cfbf6b465e6e7e3e0358f867fd48d50e055a0a
yunsseong <aster20@hanyang.ac.kr>
2025-02-27 21:20:04 +0900

    fix: 실사용 예제로 변경

--- 001-understand-ioc-and-di-concepts/src/main/java/com/spring6_study/section03_role_and_working_principle_of_ioc_container/simple_ioc_containe_v1/HumanResourceApplication.java
+++ 001-understand-ioc-and-di-concepts/src/main/java/com/spring6_study/section03_role_and_working_principle_of_ioc_container/simple_ioc_containe_v1/HumanResourceApplication.java
@@ -1,13 +1,17 @@
 package com.spring6_study.section03_role_and_working_principle_of_ioc_container.simple_ioc_containe_v1;
 
+import com.spring6_study.section03_role_and_working_principle_of_ioc_container.simple_ioc_containe_v1.domain.User;
 import com.spring6_study.section03_role_and_working_principle_of_ioc_container.simple_ioc_containe_v1.ioc_container.MyApplicationContext;
-import com.spring6_study.section03_role_and_working_principle_of_ioc_container.simple_ioc_containe_v1.service.ServiceA;
-import com.spring6_study.section03_role_and_working_principle_of_ioc_container.simple_ioc_containe_v1.service.ServiceB;
+import com.spring6_study.section03_role_and_working_principle_of_ioc_container.simple_ioc_containe_v1.service.UserRepository;
+import com.spring6_study.section03_role_and_working_principle_of_ioc_container.simple_ioc_containe_v1.service.UserService;
 
-public class Application {
+public class HumanResourceApplication {
     public static void main(String[] args) {
-        MyApplicationContext context = new MyApplicationContext(ServiceA.class, ServiceB.class);
-        ServiceA serviceA = context.getBean(ServiceA.class);
-        serviceA.useB();
+        MyApplicationContext context = new MyApplicationContext(UserRepository.class, UserService.class);
+        UserService userService = context.getBean(UserService.class);
+        User registeredUser = userService.registerUser(1L, "홍길동");
+        System.out.println("registeredUser = " + registeredUser);
+        User foundUser = userService.findUser(1L);
+        System.out.println("foundUser = " + foundUser);
     }
 }
